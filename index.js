4fdf562bd50fd9ad5904d282e49750a809f8f980
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-07-20 13:45:07 +0800

    Form submit btn work

--- index.js
+++ index.js
@@ -2,22 +2,41 @@ const emailInput = document.querySelector('#email');
 const countryInput = document.querySelector('#country');
 const postalInput = document.querySelector('#postal');
 const passwordInput = document.querySelector('#password');
-const confirmPassInput = document.querySelector('#confirm-password')
+const confirmPassInput = document.querySelector('#confirm-password');
+const submitBtn = document.querySelector('button');
 
 
 function formValidation (){
+
+    const validationState = {
+        email: false,
+        country: false,
+        postal: false,
+        password: false,
+        confirmPass: false,
+    }
     const checkEmail = (email) => {
         email.addEventListener ('input', () => {
             if (email.value !== '' && !email.value.includes('@') || !email.value.includes('.')){
                 document.getElementById('email-error').textContent = "email is invalid";
-            } else document.getElementById('email-error').textContent = "";
+                validationState.email = false;
+                console.log(validationState.email);
+            } else {
+                validationState.email = true;
+                console.log(validationState.email);
+                document.getElementById('email-error').textContent = "";
+            }
         })
     }
     const checkCountry = (country) => {
         country.addEventListener ('input', () => {
             if (country.value === 'Select Country'){
                 document.getElementById('country-error').textContent = "Please select country";
-            } else document.getElementById('country-error').textContent = "";
+                validationState.country = false;
+            } else {
+                document.getElementById('country-error').textContent = "";
+                validationState.country = true;
+            }
         })
     }
 
@@ -25,9 +44,15 @@ function formValidation (){
         postal.addEventListener ('input', () => {
             if ( !Number(postal.value)){
                 document.getElementById('postal-error').textContent = "Please insert four digits (e.g. 1234)";
+                validationState.postal = false;
             } else if (postal.value === ''){
                 document.getElementById('postal-error').textContent = "Please insert your postal";
-            } else document.getElementById('postal-error').textContent = "";
+                validationState.postal = false;
+            } else {
+                document.getElementById('postal-error').textContent = "";
+                validationState.postal = true;
+            }
+
         })
     }
 
@@ -35,7 +60,11 @@ function formValidation (){
         password.addEventListener ('input', () => {
             if (password.value.length < 10){
                 document.getElementById('password-error').textContent = "Password must be 10 characters long";
-            } else document.getElementById('password-error').textContent = "";
+                validationState.password = false;
+            } else {
+                document.getElementById('password-error').textContent = "";
+                validationState.password = true; // ✅ ADD THIS
+            }
         })
     }
 
@@ -44,23 +73,47 @@ function formValidation (){
         confirmPass.addEventListener ('input', () => {
             if (confirmPass.value !== password.value){
                 document.getElementById('confirm-password-error').textContent = "Password does not match";
-            } else document.getElementById('confirm-password-error').textContent = "";
+                validationState.confirmPass = false;
+            } else {
+                document.getElementById('confirm-password-error').textContent = "";
+                validationState.confirmPass = true;
+            }
         })
     }
+
+    const checkFormValidity = (submitBtn) => {
+        submitBtn.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            if (validationState.email === false || validationState.country === false || validationState.postal === false
+                || validationState.password === false || validationState.confirmPass === false
+            ){
+                console.log(validationState.email);
+                alert('please fill up all the details correctly');
+            } else alert ('form submitted!');
+            
+        })
+    }
+    
     return {
         checkEmail,
         checkCountry,
         checkPostal,
         checkPassword,
-        checkConfirmPassword
+        checkConfirmPassword,
+        checkFormValidity
     }
 }
 
-formValidation().checkEmail(emailInput);
-formValidation().checkCountry(countryInput);
-formValidation().checkPostal(postalInput);
-formValidation().checkPassword(passwordInput);
-formValidation().checkConfirmPassword(passwordInput, confirmPassInput);
+const validator = formValidation();
+
+validator.checkEmail(emailInput);
+validator.checkCountry(countryInput);
+validator.checkPostal(postalInput);
+validator.checkPassword(passwordInput);
+validator.checkConfirmPassword(passwordInput, confirmPassInput);
+validator.checkFormValidity(submitBtn);
+
 
 
 
