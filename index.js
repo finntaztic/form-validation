fc9043370d015aabc0e62ca4428e11d426fbaa68
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-07-20 13:02:56 +0800

    Finish validation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const emailInput = document.querySelector('#email');
 const countryInput = document.querySelector('#country');
 const postalInput = document.querySelector('#postal');
-
+const passwordInput = document.querySelector('#password');
+const confirmPassInput = document.querySelector('#confirm-password')
 
 
 function formValidation (){
@@ -22,25 +23,44 @@ function formValidation (){
 
     const checkPostal = (postal) => {
         postal.addEventListener ('input', () => {
-
-            console.log( !isNaN (postal.value));
-            if ( isNaN (postal.value)){
+            if ( !Number(postal.value)){
                 document.getElementById('postal-error').textContent = "Please insert four digits (e.g. 1234)";
-            } else if (postal.value !== ''){
+            } else if (postal.value === ''){
                 document.getElementById('postal-error').textContent = "Please insert your postal";
             } else document.getElementById('postal-error').textContent = "";
         })
     }
 
+    const checkPassword = (password) => {
+        password.addEventListener ('input', () => {
+            if (password.value.length < 10){
+                document.getElementById('password-error').textContent = "Password must be 10 characters long";
+            } else document.getElementById('password-error').textContent = "";
+        })
+    }
+
+
+    const checkConfirmPassword = (password, confirmPass) => {
+        confirmPass.addEventListener ('input', () => {
+            if (confirmPass.value !== password.value){
+                document.getElementById('confirm-password-error').textContent = "Password does not match";
+            } else document.getElementById('confirm-password-error').textContent = "";
+        })
+    }
     return {
         checkEmail,
         checkCountry,
-        checkPostal
-
+        checkPostal,
+        checkPassword,
+        checkConfirmPassword
     }
 }
 
 formValidation().checkEmail(emailInput);
 formValidation().checkCountry(countryInput);
 formValidation().checkPostal(postalInput);
+formValidation().checkPassword(passwordInput);
+formValidation().checkConfirmPassword(passwordInput, confirmPassInput);
+
+
 
